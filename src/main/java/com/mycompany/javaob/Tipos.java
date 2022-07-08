e3ecef7620a6afe9ce8b62f619fa254d28e218c3
DuvanGG <duvanga@hotmail.es>
2022-07-08 15:12:42 -0500

    tipos de datos java

---
+++ src/main/java/com/mycompany/javaob/Tipos.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javaob;
+
+/**
+ *
+ * @author duvan
+ */
+public class Tipos {
+    public static void main(String[] args) {
+        
+        //Tipos de datos
+        
+        /*
+            Los datos en minuscula son tipos de 
+            datos primitivos, los que estan en 
+            mayuscula son tipos de datos que 
+            envuelven de clases.
+        */
+        
+        //Numericos decimales
+        byte numero1 = 3;
+        System.out.println(numero1);
+        short numero2 = 6;
+        System.out.println(numero2);
+        int numero3 = 9;
+        System.out.println(numero3);
+        long numero4 = 12;
+        System.out.println(numero4);
+        //Numerico flotantes
+        float numero5 = 3.3f;
+        System.out.println(numero5);
+        double numero6 = 6.6d;
+        System.out.println(numero6);
+        //Boleanos
+        boolean variableBool = true;
+        System.out.println(variableBool);
+        boolean boolVariable = false;
+        System.out.println(boolVariable);
+        //Texto
+        char varText = 'c';
+        System.out.println(varText);
+        String textVar = "Hola mundo";
+        System.out.println(textVar);
+               
+        
+    }
+    
+}
